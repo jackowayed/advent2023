c135aceacc9e97eb9d438c362204bc28ae329000
Daniel Jackoway <commits@danieljackoway.com>
2023-12-01 21:17:44 -0800

    2p1

---
+++ 2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import collections
+import fileinput
+import re
+
+def parse_round(s):
+    round = collections.defaultdict(int)
+    for part in s.split(","):
+        m = re.match(r" (\d+) (\w+)", part)
+        round[m.group(2)] = int(m.group(1))
+    return round
+
+def impossible(round):
+    return round["red"] > 12 or round["green"] > 13 or round["blue"] > 14
+
+def part1():
+    # [line.strip() for line in fileinput.input()]
+    answer = []
+    for line in fileinput.input():
+        gid = int(re.match(r"^Game (\d+):", line).group(1))
+        parts = line.split(":")[1].split(";")
+        #import pdb
+        #pdb.set_trace()
+        if sum(impossible(parse_round(p)) for p in parts) == 0:
+            answer.append(gid)
+    print(answer)
+    return sum(answer)
+
+
+def part2():
+    # [line.strip() for line in fileinput.input()]
+    for line in fileinput.input():
+        pass
+    return
+
+print(part1())
+#print(part2())
