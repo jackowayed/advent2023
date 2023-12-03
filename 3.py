aa5ce06de3ef6840aa5e95757aaa42529ec30acb
Daniel Jackoway <commits@danieljackoway.com>
2023-12-02 21:47:06 -0800

    ugh

--- 3.py
+++ 3.py
@@ -31,12 +31,40 @@ def part1():
                 total += int(match.group(0))
     return total
 
+def safe_check_star(x, y, grid):
+    if x < 0 or y < 0 or y >= len(grid) or x >= len(grid[y]):
+        return False
+    return grid[y][x] == "*"
+
+def adjacent_stars(y, match, grid):
+    x_start = match.start()
+    x_end = match.end()
+    candidates = [(x_start - 1, y), (x_end, y)]
+    for _x in range(x_start - 1, x_end +1):
+        candidates.append((_x, y - 1))
+        candidates.append((_x, y + 1))
+    return [(x, y) for x, y in candidates if safe_check_star(x, y, grid)]
+
 
 def part2():
-    # [line.strip() for line in fileinput.input()]
-    for line in fileinput.input():
-        pass
-    return
+    grid = [line.strip() for line in fileinput.input()]
+    star_adjacencies = collections.defaultdict(list)
+    total = 0
+    for outer_y, line in enumerate(grid):
+        for match in re.finditer(r"\d+", line):
+            for x, y in adjacent_stars(outer_y, match, grid):
+                star_adjacencies[(x, y)].append(int(match.group(0)))
+    for v in star_adjacencies.values():
+        print(v)
+        if len(v) == 2:
+            total += v[0] * v[1]
+    return total
+    #print(star_adjacencies)
+    lines = []
+    for k, v in star_adjacencies.items():
+        lines.append(f"{k[1], k[0]}: {v}")
+    for l in sorted(lines):
+        print(l)
 
-print(part1())
-#print(part2())
+#print(part1())
+print(part2())
