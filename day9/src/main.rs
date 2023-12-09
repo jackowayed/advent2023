181da10280902841088284b57f136eb8011b6887
Daniel Jackoway <commits@danieljackoway.com>
2023-12-09 11:52:47 -0800

    9p2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -25,6 +25,23 @@ fn extrapolate_sequence(seq: &Vec<i64>) -> i64 {
     // //extrapolated_vals.iter().sum()
 }
 
+fn extrapolate_backwards(seq: &Vec<i64>) -> i64 {
+    let mut sequences = vec![seq.to_vec()];
+    while sequences.last().unwrap().iter().any(|i| *i != 0) {
+        let cur_seq = sequences.last().unwrap();
+        sequences.push(
+            (1..cur_seq.len())
+                .map(|i| cur_seq[i] - cur_seq[i - 1])
+                .collect(),
+        )
+    }
+    //sequences[0][0] * 2 - sequences.iter().map(|seq| seq.last().unwrap()).sum::<i64>()
+    let mut extrapolations = vec![0i64];
+    for s in sequences.iter().rev() {
+        extrapolations.push(s[0] - extrapolations.last().unwrap());
+    }
+    *extrapolations.last().unwrap()
+}
 fn main() {
     let lines = include_str!("9.in.txt").lines();
     let sequences: Vec<Vec<i64>> = lines
@@ -35,6 +52,10 @@ fn main() {
         })
         .collect();
     //let answer: Vec<i64> = sequences.iter().map(extrapolate_sequence).collect();
-    let answer: i64 = sequences.iter().map(extrapolate_sequence).sum();
-    println!("{:?}", answer)
+    let part_1_answer: i64 = sequences.iter().map(extrapolate_sequence).sum();
+    println!("{:?}", part_1_answer);
+    println!(
+        "{:?}",
+        sequences.iter().map(extrapolate_backwards).sum::<i64>()
+    );
 }
