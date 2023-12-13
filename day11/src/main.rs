7491427e1aa4b8df686aec46ab7aae1238448df9
Daniel Jackoway <commits@danieljackoway.com>
2023-12-12 19:15:53 -0800

    11p2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -6,38 +6,40 @@ fn print_grid(grid: &Vec<Vec<u8>>) {
     }
 }
 
+type Galaxy = (usize, usize);
+
+fn axis_distance(p1: usize, p2: usize, empty_ranks: &Vec<usize>) -> usize {
+    if p1 > p2 {
+        return axis_distance(p2, p1, empty_ranks);
+    }
+    (p2 - p1) + empty_ranks.iter().filter(|r| **r > p1 && **r < p2).count() * 999999
+}
+
 fn main() {
-    let mut wip_grid: Vec<Vec<u8>> = Vec::new();
-    for line in include_str!("11.in.txt")
+    let grid: Vec<&[u8]> = 
+    //include_str!("11.test")
+        include_str!("11.in.txt")
         .lines()
-        .map(|s| s.trim().as_bytes().to_vec())
-    {
-        if line.iter().all(|c| *c == b'.') {
-            wip_grid.push(line.clone());
-        }
-        wip_grid.push(line);
-    }
-    let mut grid: Vec<Vec<u8>> = vec![Vec::new(); wip_grid.len()];
-    for x in 0..wip_grid[0].len() {
-        let times = if (0..wip_grid.len()).all(|y| wip_grid[y][x] == b'.') {
-            2
-        } else {
-            1
-        };
-        for _ in 0..times {
-            for y in 0..wip_grid.len() {
-                grid[y].push(wip_grid[y][x]);
-            }
-        }
-    }
-    print_grid(&grid);
-    //let mut galaxies: Vec<(usize, usize)> = Vec::new();
-    let galaxies: Vec<(usize, usize)> = iproduct!(0..grid.len(), 0..grid[0].len())
+        .map(|s| s.trim().as_bytes())
+        .collect();
+    let empty_rows = (0..grid.len())
+        .filter(|y| grid[*y].iter().all(|c| *c == b'.'))
+        .collect();
+    let empty_cols = (0..grid[0].len())
+        .filter(|x| (0..grid.len()).all(|y| grid[y][*x] == b'.'))
+        .collect();
+    let galaxies: Vec<Galaxy> = iproduct!(0..grid.len(), 0..grid[0].len())
         .filter(|(y, x)| grid[*y][*x] != b'.')
         .collect();
-    let distance = iproduct!(galaxies.clone(), galaxies.clone())
-        .map(|((y1, x1), (y2, x2))| (y2 as i32 - y1 as i32).abs() + (x2 as i32 - x1 as i32).abs())
-        .sum::<i32>()
+
+    // let distance = |g1: Galaxy, g2: Galaxy| -> usize {
+    //     axis_distance(g1.0, g2.0, empty_rows) + axis_distance(g1.1, g2.1, empty_cols)
+    // }
+    let total_distance: usize = iproduct!(galaxies.clone(), galaxies.clone())
+        .map(|(g1, g2)| {
+            axis_distance(g1.0, g2.0, &empty_rows) + axis_distance(g1.1, g2.1, &empty_cols)
+        })
+        .sum::<usize>()
         / 2;
-    println!("{:?}", distance);
+    println!("{:?}", total_distance);
 }
