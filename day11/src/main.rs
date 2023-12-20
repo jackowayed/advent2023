3e24b3b2f13f696b0da5d8cfa16b5b0ab11869a7
Daniel Jackoway <commits@danieljackoway.com>
2023-12-20 13:01:35 -0800

    use &s for closure variables instead of *s

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -12,7 +12,7 @@ fn axis_distance(p1: usize, p2: usize, empty_ranks: &Vec<usize>) -> usize {
     if p1 > p2 {
         return axis_distance(p2, p1, empty_ranks);
     }
-    (p2 - p1) + empty_ranks.iter().filter(|r| **r > p1 && **r < p2).count() * 999999
+    (p2 - p1) + empty_ranks.iter().filter(|&&r| r > p1 && r < p2).count() * 999999
 }
 
 fn main() {
@@ -23,13 +23,13 @@ fn main() {
         .map(|s| s.trim().as_bytes())
         .collect();
     let empty_rows = (0..grid.len())
-        .filter(|y| grid[*y].iter().all(|c| *c == b'.'))
+        .filter(|&y| grid[y].iter().all(|&c| c == b'.'))
         .collect();
     let empty_cols = (0..grid[0].len())
-        .filter(|x| (0..grid.len()).all(|y| grid[y][*x] == b'.'))
+        .filter(|&x| (0..grid.len()).all(|y| grid[y][x] == b'.'))
         .collect();
     let galaxies: Vec<Galaxy> = iproduct!(0..grid.len(), 0..grid[0].len())
-        .filter(|(y, x)| grid[*y][*x] != b'.')
+        .filter(|&(y, x)| grid[y][x] != b'.')
         .collect();
 
     // let distance = |g1: Galaxy, g2: Galaxy| -> usize {
